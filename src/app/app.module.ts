d3f4046d0148a97d3d721579d6b75771564370f5
Alexandra Pavel <alexandrapavel.ct@gmail.com>
2017-06-13 15:00:25 +0300

    Checking seed project

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+     { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { NgModule }      from "@angular/core";
+import { Routes, RouterModule }     from "@angular/router";
+import { AppComponent }       from "./app.component";
+
+const appRoutes: Routes = [
+    { path: "", component: AppComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+  ]
+})
+export class AppRoutingModule { }
+
